495e4f9f11659632c42f60b2eb95c05361654c7f
andrii_kl <andriikliui@gmail.com>
2023-09-23 12:42:02 +0300

    add new file to keep only addresses

--- utils/GenerateWallets.js
+++ utils/GenerateWallets.js
@@ -11,11 +11,15 @@ export default async function GenerateWallets(walletName,counter,flag) {
         const fileExists = await fs.pathExists('./data/generated.csv');
         if (!fileExists) {
             await fs.writeFile('./data/generated.csv', 'WalletName,Address,Mnemonic,PrivateKey\n');
+            await fs.writeFile('./data/addresses.txt', '');
             await Generate(walletName,counter)
         } else {
             if (flag === 'new') {
                 await fs.unlink('./data/generated.csv');
                 await fs.writeFile('./data/generated.csv', 'WalletName,Address,Mnemonic,PrivateKey\n');
+
+                await fs.unlink('./data/addresses.txt');
+                await fs.writeFile('./data/addresses.txt', '');
                 await Generate(walletName,counter)
             }
             if (flag !== 'new') {console.log('File ./data/generated.csv already exist, if you want to genetare new wallets, use:\nnpm run generate -- argent 30 new')}
@@ -25,6 +29,7 @@ export default async function GenerateWallets(walletName,counter,flag) {
 
 const Generate = async (walletName,counter) =>{
     const stream = fs.createWriteStream('./data/generated.csv', {flags: 'a'});
+    const addrStream = fs.createWriteStream('./data/addresses.txt', {flags: 'a'});
 
     for (let i = 0; i<counter;i++){
         const mnemonic = await MnemonicGenerator.generateMnemonicPhrase()
@@ -32,9 +37,11 @@ const Generate = async (walletName,counter) =>{
         const address = await getAddress(privateKey,walletName);
 
         stream.write(`${walletName},${address},${mnemonic},${privateKey}\n`);
+        addrStream.write(`${address}\n`);
     }
 
     stream.end();
+    addrStream.end();
     console.log('Data Saved successfully to generated.csv');
     console.log("Don't forget to add addresses to OKX white list")
 }
